8e508d2846663effd87753bce269d9ee0a461173
frankieavina <frankieavina@gmail.com>
2022-09-14 16:49:09 -0700

    connection with Railwayt: succes

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql2/promise');
 
 require('dotenv').config();
 
-const port = process.env.PORT; 
+const port = process.env.PORT;
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+})
+
+
+app.use(async function mysqlConnection(req, res, next) {
+    try {
+      req.db = await pool.getConnection();
+      req.db.connection.config.namedPlaceholders = true;
+  
+      await next();
+  
+      req.db.release();
+    } catch (err) {
+      console.log(err)
+      if (req.db) req.db.release();
+      throw err;
+    }
+});
 
 app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.get("/", (req, res) => {
-  res.json({ message: "ok" });
+app.use(express.urlencoded({extended: true,}));
+
+app.get("/", async (req, res) => {
+  try{
+    const [users] = await req.db.query(`SELECT * FROM users`);
+    res.json(users);
+  }catch(err){
+    console.error(err);
+  }
 });
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
